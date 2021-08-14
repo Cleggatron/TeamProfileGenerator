27c392d4c4dc8213a8571907588506f292bee7b8
Cleggatron <david.robert.clegg@gmail.com>
2021-08-14 18:43:42 +0100

    wrote engineer specific tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,38 +1,45 @@
 const Engineer = require("../lib/Engineer.js");
 
 describe("Engineer", () =>{
-    it("should create a new object containing name, id, and email properties when called", () =>{
-        const testEngineer = new Engineer("John", 4, "John@testemail.com");
+    it("should create a new object containing name, id, email, and guthub properties when called", () =>{
+        const testEngineer = new Engineer("John", 4, "John@testemail.com", "johnGithub");
 
         expect("name" in testEngineer).toEqual(true); 
         expect("id" in testEngineer).toEqual(true); 
         expect("email" in testEngineer).toEqual(true);  
         expect("role" in testEngineer).toEqual(true);  
+        expect("github" in testEngineer).toEqual(true);
     });
 
     it("should return the correct name when getName() is called", () =>{
-        const testEngineer = new Engineer("John", 4, "John@testemail.com")
+        const testEngineer = new Engineer("John", 4, "John@testemail.com", "johnGithub")
 
         expect(testEngineer.getName()).toEqual(testEngineer.name);
     });
     
     it("should return the correct id when getId() is called", () =>{
-        const testEngineer = new Engineer("John", 4, "John@testemail.com")
+        const testEngineer = new Engineer("John", 4, "John@testemail.com", "johnGithub")
 
         expect(testEngineer.getId()).toEqual(testEngineer.id);
     });
     
     it("should return the correct name when getEmail() is called", () =>{
-        const testEngineer = new Engineer("John", 4, "John@testemail.com")
+        const testEngineer = new Engineer("John", 4, "John@testemail.com", "johnGithub")
 
         expect(testEngineer.getEmail()).toEqual(testEngineer.email);
     });
 
     it("should return the employee when getRole() is called", () =>{
-        const testEngineer = new Engineer("John", 4, "John@testemail.com")
+        const testEngineer = new Engineer("John", 4, "John@testemail.com", "johnGithub")
 
         expect(testEngineer.getRole()).toEqual("Engineer");
     });
+
+    it("should return the engineer's github username when getGithub() is called", () => {
+        const testEngineer = new Engineer("John", 4, "John@testemail.com", "johnGithub")
+
+        expect(testEngineer.getGithub()).toEqual(testEngineer.github);
+    })
     
     
 })
